302953ebbea811c67effdc51c15f250db7a7623d
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2023-05-31 18:16:49 +0300

    feat(helpers): ✨ added project helpers

---
+++ src/helpers/utilities.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'stylelint';
+
+export function defineConfig(
+  config: Config,
+): Config {
+  return config;
+}
+
+export function defineRules(
+  rules: Config['rules'],
+): Config['rules'] {
+  return rules;
+}
