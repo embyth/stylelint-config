cf1e8b426043fb57e93c29eb09482ee856a48d48
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2023-09-06 16:06:01 +0300

    feat(config): ✨ ⚙️ unified configs into single one

--- src/configs/core.ts
+++ src/configs/core.ts
@@ -1,21 +1,47 @@
+import scssSyntax from 'postcss-scss';
+
+// helpers
 import { defineConfig } from '../helpers/utilities';
+
+// rules
 import { bemRules } from '../rules/bem';
 import { deprecatedRules } from '../rules/deprecated';
 import { generalRules } from '../rules/general';
 import { orderRules } from '../rules/order';
+import { prettierRules } from '../rules/prettier';
+import { scssRules } from '../rules/scss';
 
 export const coreConfig = defineConfig({
-  plugins: ['stylelint-selector-bem-pattern', 'stylelint-order'],
+  plugins: [
+    'stylelint-prettier',
+    'stylelint-selector-bem-pattern',
+    'stylelint-order',
+  ],
   extends: ['stylelint-config-recommended'],
+  reportNeedlessDisables: true,
+  reportInvalidScopeDisables: true,
   rules: {
     ...generalRules,
-    /**
-     * When we will bump stylelint to the next major version this rules will be removed,
-     * so we need to add them to the stylistic plugin
-     * and add "stylelint-stylistic" to the plugins array
-    */
     ...deprecatedRules,
     ...bemRules,
     ...orderRules,
+    ...prettierRules,
   },
+  overrides: [
+    {
+      files: ['**/*.scss'],
+      plugins: ['stylelint-scss'],
+      ignoreFiles: [
+        '**/*.js',
+        '**/*.jsx',
+        '**/*.tsx',
+        '**/*.ts',
+        '**/*.json',
+      ],
+      customSyntax: scssSyntax,
+      rules: {
+        ...scssRules,
+      },
+    },
+  ],
 });
