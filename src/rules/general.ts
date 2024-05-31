0a59ea438095f131b3798cf9e1951dc2b1f70891
Rostyslav Miniukov <miniukovrostyslav@gmail.com>
2024-05-31 23:23:00 +0300

    feat(rules): ✨ added selector-class-pattern with bem pattern to general rules

--- src/rules/general.ts
+++ src/rules/general.ts
@@ -346,4 +346,19 @@ export const generalRules = defineRules({
       ignoreAtRules: ['else'],
     },
   ],
+  /**
+   * Specify a pattern for class selectors.
+   * @link https://stylelint.io/user-guide/rules/selector-class-pattern/
+   */
+  'selector-class-pattern': [
+    '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+    {
+      /** This option will resolve nested selectors with & interpolation. - https://stylelint.io/user-guide/rules/selector-class-pattern/#resolvenestedselectors-true--false-default-false */
+      resolveNestedSelectors: true,
+      /** Custom message */
+      message: function expected(selectorValue: string) {
+        return `Expected class selector "${selectorValue}" to match BEM CSS pattern https://en.bem.info/methodology/css. Selector validation tool: https://regexr.com/3apms`;
+      },
+    },
+  ],
 });
